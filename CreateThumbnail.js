3271d267c51a465868e579b6f87167c5ccb2c53e
Bala.Kumaran <kbalamur@amazon.com>
2018-08-14 16:42:18 +1000

    console.log

--- CreateThumbnail.js
+++ CreateThumbnail.js
@@ -15,6 +15,7 @@ exports.handler = function(event, context) {
 	// Read options from the event.
 	console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
 	var srcBucket = event.Records[0].s3.bucket.name;
+	console.log("srcBucket : " + srcBucket);
 	// Object key may have spaces or unicode non-ASCII characters.
     var srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
 	var dstBucket = srcBucket + "resized";
